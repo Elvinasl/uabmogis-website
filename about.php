f2b234b5e6b2501796c8548b77b532967615859f
Elvinas Lukasevicius <ell@open.ch>
2019-09-12 10:50:36 +0300

    Remove .idea from repo

--- about.php
+++ about.php
@@ -22,7 +22,7 @@
 
     <?php include "header.php" ?>
     <!--====================  breadcrumb area ====================-->
-    <div class="breadcrumb-area bg-img" data-bg="assets/img/backgrounds/funfact-bg.jpg">
+    <div class="breadcrumb-area bg-img">
         <div class="container">
             <div class="row">
                 <div class="col">
@@ -52,11 +52,9 @@
 
                     <div class="col-lg-6 col-12 mb-30">
                         <div class="about-content-two">
-                            <h3>Welcome to Constrk</h3>
-                            <h1>50 Years of Experience in Industry</h1>
-                            <h4>We are ready to build your dream home Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum, beatae.</h4>
+                            <h1>5 metu patirtis statybose!!! ss</h1>
                             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam repudiandae odit dolorum quis laudantium impedit beatae perferendis natus, hic libero sed atque quibusdam possimus error, voluptate est molestiae doloremque necessitatibus illum rerum sunt! Ad sunt obcaecati voluptatem sint sequi quos, qui non deleniti a praesentium, sapiente accusantium odit.</p>
-                            <a href="service.html" class="ht-btn--default ht-btn--default--dark-hover section-space--top--20">Our Services</a>
+                            <a href="service.html" class="ht-btn--default ht-btn--default--dark-hover section-space--top--20">Mūsų darbai</a>
                         </div>
                     </div>
 
@@ -119,27 +117,15 @@
 
                     <div class="col-sm-6 col-12 order-1 order-lg-2">
                         <div class="about-image about-image-1">
-                            <img src="assets/img/about/about-1.jpg" alt="">
+                            <img src="assets/img/about/about-1.jpeg" class="img-thumbnail" alt="Apie Mogis UAB">
                         </div>
                     </div>
 
-                    <div class="col-sm-6 col-12 order-2 order-lg-3">
-                        <div class="about-image about-image-2">
-                            <img src="assets/img/about/about-2.jpg" alt="">
-                        </div>
-                    </div>
 
                     <div class="col-lg-6 col-12 order-3 order-lg-1">
                         <div class="about-content about-content-1">
-                            <h1><span>50</span>Years of Experience</h1>
+                            <h1><span>5</span>metu patirties tekstas</h1>
                             <p>ligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor ellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum atibus saepe eveniet ut et voluptates repudiandae sint et molestiae</p>
-                        </div>
-                    </div>
-
-                    <div class="col-lg-6 col-12 order-4">
-                        <div class="about-content about-content-2">
-                            <p>ligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor ellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum atibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapie</p>
-                            <a href="contact.php" class="ht-btn--default ht-btn--default--dark-hover section-space--top--20">Contact us</a>
                         </div>
                     </div>
 
@@ -148,7 +134,7 @@
         </div>
         <!--About section end-->
         <!--====================  fun fact area ====================-->
-        <div class="funfact-section section-space--inner--100 bg-img" data-bg="assets/img/backgrounds/funfact-bg.jpg">
+        <div class="funfact-section section-space--inner--100 bg-img">
             <div class="container">
                 <div class="row">
                     <div class="col-lg-12">
@@ -158,25 +144,25 @@
                                 <div class="single-fact col-md-3 col-6 section-space--bottom--30">
                                     <img src="assets/img/icons/funfact-project.png" alt="">
                                     <h1 class="counter">598</h1>
-                                    <h4>Project</h4>
+                                    <h4>Projektai</h4>
                                 </div>
 
                                 <div class="single-fact col-md-3 col-6 section-space--bottom--30">
                                     <img src="assets/img/icons/funfact-clients.png" alt="">
                                     <h1 class="counter">128</h1>
-                                    <h4>Clients</h4>
+                                    <h4>Laimingi užsakovai</h4>
                                 </div>
 
                                 <div class="single-fact col-md-3 col-6 section-space--bottom--30">
                                     <img src="assets/img/icons/funfact-success.png" alt="">
                                     <h1 class="counter">114</h1>
-                                    <h4>Success</h4>
+                                    <h4>Kazkas</h4>
                                 </div>
 
                                 <div class="single-fact col-md-3 col-6 section-space--bottom--30">
                                     <img src="assets/img/icons/funfact-award.png" alt="">
                                     <h1 class="counter">109</h1>
-                                    <h4>Awards</h4>
+                                    <h4>Kazkas</h4>
                                 </div>
                             </div>
                         </div>
@@ -185,150 +171,6 @@
             </div>
         </div>
         <!--====================  End of fun fact area  ====================-->
-        <!--====================  testimonial slider area ====================-->
-        <div class="testimonial-slider-area testimonial-slider-area-bg section-space--inner--120 bg-img" data-bg="assets/img/backgrounds/testimonial.jpg">
-            <div class="container">
-                <div class="row">
-                    <div class="col-lg-10 m-auto">
-                        <div class="testimonial-slider">
-                            <div class="testimonial-slider__container-area">
-                                <div class="swiper-container testimonial-slider__container">
-                                    <div class="swiper-wrapper testimonial-slider__wrapper">
-                                        <div class="swiper-slide testimonial-slider__single-slide">
-                                            <div class="author">
-                                                <div class="author__image">
-                                                    <img src="assets/img/testimonial/1.jpg" alt="">
-                                                </div>
-                                                <div class="author__details">
-                                                    <h4 class="name">Madison Black</h4>
-                                                    <div class="designation">Founder</div>
-                                                </div>
-                                            </div>
-                                            <div class="content">
-                                                Lorem ipsum dolor sit amet, consectetur adipisi elit sed do eiusmod tempor incididu ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                                            </div>
-                                        </div>
-                                        <div class="swiper-slide testimonial-slider__single-slide">
-                                            <div class="author">
-                                                <div class="author__image">
-                                                    <img src="assets/img/testimonial/2.jpg" alt="">
-                                                </div>
-                                                <div class="author__details">
-                                                    <h4 class="name">Madison Black</h4>
-                                                    <div class="designation">Founder</div>
-                                                </div>
-                                            </div>
-                                            <div class="content">
-                                                Lorem ipsum dolor sit amet, consectetur adipisi elit sed do eiusmod tempor incididu ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                                            </div>
-                                        </div>
-                                        <div class="swiper-slide testimonial-slider__single-slide">
-                                            <div class="author">
-                                                <div class="author__image">
-                                                    <img src="assets/img/testimonial/3.jpg" alt="">
-                                                </div>
-                                                <div class="author__details">
-                                                    <h4 class="name">Madison Black</h4>
-                                                    <div class="designation">Founder</div>
-                                                </div>
-                                            </div>
-                                            <div class="content">
-                                                Lorem ipsum dolor sit amet, consectetur adipisi elit sed do eiusmod tempor incididu ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="swiper-pagination swiper-pagination-3"></div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!--====================  End of testimonial slider area  ====================-->
-        <!--====================  brand logo area ====================-->
-        <div class="brand-logo-slider-area section-space--inner--60">
-            <div class="container">
-                <div class="row">
-                    <div class="col-lg-12">
-                        <!-- brand logo slider -->
-                        <div class="brand-logo-slider__container-area">
-                            <div class="swiper-container brand-logo-slider__container">
-                                <div class="swiper-wrapper brand-logo-slider__wrapper">
-                                    <div class="swiper-slide brand-logo-slider__single">
-                                        <div class="image">
-                                            <a href="#">
-                                                <img src="assets/img/brand-logo/1.png" class="img-fluid" alt="">
-                                            </a>
-                                        </div>
-
-                                    </div>
-                                    <div class="swiper-slide brand-logo-slider__single">
-                                        <div class="image">
-                                            <a href="#">
-                                                <img src="assets/img/brand-logo/2.png" class="img-fluid" alt="">
-                                            </a>
-                                        </div>
-
-                                    </div>
-                                    <div class="swiper-slide brand-logo-slider__single">
-                                        <div class="image">
-                                            <a href="#">
-                                                <img src="assets/img/brand-logo/3.png" class="img-fluid" alt="">
-                                            </a>
-                                        </div>
-
-                                    </div>
-                                    <div class="swiper-slide brand-logo-slider__single">
-                                        <div class="image">
-                                            <a href="#">
-                                                <img src="assets/img/brand-logo/4.png" class="img-fluid" alt="">
-                                            </a>
-                                        </div>
-
-                                    </div>
-                                    <div class="swiper-slide brand-logo-slider__single">
-                                        <div class="image">
-                                            <a href="#">
-                                                <img src="assets/img/brand-logo/1.png" class="img-fluid" alt="">
-                                            </a>
-                                        </div>
-
-                                    </div>
-                                    <div class="swiper-slide brand-logo-slider__single">
-                                        <div class="image">
-                                            <a href="#">
-                                                <img src="assets/img/brand-logo/2.png" class="img-fluid" alt="">
-                                            </a>
-                                        </div>
-
-                                    </div>
-                                    <div class="swiper-slide brand-logo-slider__single">
-                                        <div class="image">
-                                            <a href="#">
-                                                <img src="assets/img/brand-logo/3.png" class="img-fluid" alt="">
-                                            </a>
-                                        </div>
-
-                                    </div>
-                                    <div class="swiper-slide brand-logo-slider__single">
-                                        <div class="image">
-                                            <a href="#">
-                                                <img src="assets/img/brand-logo/4.png" class="img-fluid" alt="">
-                                            </a>
-                                        </div>
-
-                                    </div>
-
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!--====================  End of brand logo area  ====================-->
-
     </div>
     <?php include "footer.php" ?>
     <!-- JS
